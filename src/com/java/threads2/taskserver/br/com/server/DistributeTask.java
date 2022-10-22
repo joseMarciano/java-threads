b1633167b160b18e5c67ee30399d683beb3dbcd3
joseMarciano <marcianojosepaulo@gmail.com>
2022-10-22 13:37:35 -0300

    feat: :sparkles: implementing the server response

--- src/com/java/threads2/taskserver/br/com/server/DistributeTask.java
+++ src/com/java/threads2/taskserver/br/com/server/DistributeTask.java
@@ -1,6 +1,6 @@
 package com.java.threads2.taskserver.br.com.server;
 
-import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -16,18 +16,30 @@ public class DistributeTask implements Runnable {
         try {
             System.out.printf("Distributing task in separated thread for socket %s%n", this.socket.getPort());
             final var inputClient = new Scanner(socket.getInputStream());
+            final var outputClient = new PrintStream(socket.getOutputStream());
 
             while (inputClient.hasNextLine()) {
                 final var command = inputClient.nextLine();
                 System.out.printf("RECEIVED OF %s -> %s%n", this.socket.getPort(), command);
+
+                extracted(outputClient, command);
+
             }
 
             inputClient.close();
-
+            outputClient.close();
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException(e);
         }
 
+    }
+
+    private static void extracted(final PrintStream outputClient, final String command) {
+        switch (command) {
+            case "c1" -> outputClient.println("COMMAND C1 RECEIVED");
+            case "c2" -> outputClient.println("COMMAND C2 RECEIVED");
+            default -> outputClient.println("COMMAND NOT FOUND");
+        }
     }
 }
